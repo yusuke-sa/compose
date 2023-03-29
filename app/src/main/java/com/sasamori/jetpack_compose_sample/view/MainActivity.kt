9dab4e8f14ba84c367d7688dae31de26e3abe55d
yusuke-sa <yusuke.sasa720.mth@gmail.com>
2023-03-29 20:19:01 +0900

    initial commit

---
+++ app/src/main/java/com/sasamori/jetpack_compose_sample/view/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.sasamori.jetpack_compose_sample.view
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MainScreen()
+        }
+    }
+
+    @Composable
+    fun MainScreen() {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            Text(
+                text = "Hello world!",
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Bold,
+            )
+        }
+    }
+
+    @Preview
+    @Composable
+    fun PreViewMainActivity() {
+        MainScreen()
+    }
+
+}
+
